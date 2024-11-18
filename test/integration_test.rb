779006d707f2fa42828a76d9e64954c662fba6b4
Luke Gruber <luke.gru@gmail.com>
2024-11-18 14:31:19 -0500

    Allow matching space characters in pattern

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -10,84 +10,84 @@ class IntegrationTest < Minitest::Test
   SUB_BIN = "./bin/sub"
 
   def test_sub_print_only
-    out, err, code = run_sub("ls -al", "l//p")
+    out, err, code = run_sub(["ls", "-al"], "l//p")
     assert_equal "s -a\n", out
     assert_equal "", err
     assert_success code
   end
 
   def test_sub_invalid_option_gives_warning
-    out, err, code = run_sub("ls -al", "l//pX")
+    out, err, code = run_sub(["ls", "-al"], "l//pX")
     assert_equal "s -a\n", out
     assert_match(/Warning: unknown flag: X\n/, err)
     assert_success code
   end
 
   def test_sub_repeated_option_gives_no_warning
-    out, err, code = run_sub("ls -al", "l//pp")
+    out, err, code = run_sub(["ls",  "-al"], "l//pp")
     assert_equal "s -a\n", out
     assert_equal "", err
     assert_success code
   end
 
   def test_sub_command_doesnt_exist
-    out, err, code = run_sub("ls -al", "l/xxx/")
+    out, err, code = run_sub(["ls",  "-al"], "l/xxx/")
     assert_equal "xxxs -axxx\n", out
     assert_match(/xxxs: command not found/, err)
     refute_success code
   end
 
   def test_sub_first_matching_word_only_f_option
-    out, err, code = run_sub("wget https://wget.com", "wget/curl/fp")
+    out, err, code = run_sub(["wget", "https://wget.com"], "wget/curl/fp")
     assert_equal "curl https://wget.com\n", out
     assert_equal "", err
     assert_success code
   end
 
   def test_sub_last_matching_word_only_l_option
-    out, err, code = run_sub("ls -al", "l//l")
+    out, err, code = run_sub(["ls", "-al"], "l//l")
     assert_match(/ls -a\n/, out)
     assert_equal "", err
     assert_success code
   end
 
   def test_sub_interpret_wildcards_in_pattern_as_literals_L_option
-    out, err, code = run_sub("cp here.txt there.txt", "./_/Lp")
+    out, err, code = run_sub(["cp", "here.txt", "there.txt"], "./_/Lp")
     assert_match(/cp here_txt there_txt\n/, out)
     assert_equal "", err
     assert_success code
   end
 
   def test_sub_pattern_ignorecase_i_option
-    out, err, code = run_sub("cp here.txt there.txt", "CP/mv/ip")
+    out, err, code = run_sub(["cp", "here.txt", "there.txt"], "CP/mv/ip")
     assert_match(/mv here.txt there.txt\n/, out)
     assert_equal "", err
     assert_success code
   end
 
   def test_sub_general_substitution_g_option
-    out, err, code = run_sub("cp here.txt there.txt", "./_/gp")
+    out, err, code = run_sub(["cp", "here.txt", "there.txt"], "./_/gp")
     assert_match(/__ ________ _________\n/, out)
     assert_equal "", err
     assert_success code
   end
 
   def test_two_substitutions_without_interference
-    out, err, code = run_sub("ls -al", "l//", "s//p")
+    out, err, code = run_sub(["ls", "-al"], "l//", "s//p")
     assert_match(/-a\s*?\n/, out)
     assert_equal "", err
     assert_success code
   end
 
   def test_multiple_substitutions_run_one_after_another
-    out, err, code = run_sub("ls -al", "l/HI/", "hi/HEY/ip")
+    out, err, code = run_sub(["ls", "-al"], "l/HI/", "hi/HEY/ip")
     assert_match(/HEYs -aHEY\n/, out)
     assert_equal "", err
     assert_success code
   end
 
   def test_global_options_can_be_given_after_substitutions
-    out, err, code = run_sub("ls -al", "l//", "/p")
+    out, err, code = run_sub(["ls", "-al"], "l//", "/p")
     assert_match(/s -a\n/, out)
     assert_equal "", err
     assert_success code
@@ -95,10 +95,18 @@ class IntegrationTest < Minitest::Test
 
   def test_patterns_can_escape_backslash
     # ./bin/sub ls / -- '\//hi /p'
-    out, err, code = run_sub("ls /", "\\//hi", "/p")
+    out, err, code = run_sub(["ls", "/"], "\\//hi", "/p")
     assert_equal "", err
     assert_success code
     assert_match(/ls hi\n/, out)
+  end
+
+  def test_pattern_can_match_space_character
+    # ./bin/sub ls '-al ' -- ' /hi' /p
+    out, err, code = run_sub(["ls", "-al "], " /hi/", "/p")
+    assert_equal "", err
+    assert_success code
+    assert_match(/ls -alhi\n/, out)
   end
 
   private
@@ -118,8 +126,8 @@ class IntegrationTest < Minitest::Test
       #[out, "", $?.exitstatus]
     #end
   else
-    def run_sub(cmdline, *subs)
-      args = cmdline.split(/\s+/)
+    def run_sub(cmdline_argv, *subs)
+      args = cmdline_argv.dup
       args << "--"
       subs.each do |sub|
         args << sub
